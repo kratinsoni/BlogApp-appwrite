d8b90ead4dde8a7e17107b98a817838073dd2ccc
Harshit Soni <kratin67soni@gmail.com>
2024-03-04 18:37:32 +0530

    configured redux and tailwind css for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import './App.css'
+import { useDispatch } from "react-redux";
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice.js";
+import { Header, Footer } from "./components/index.js";
 
 function App() {
-  
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
     <>
-      <h1>BlogApp-Appwrite</h1>
+      <div className="min-h-screen flex-wrap content-between bg-gray-400">
+        <div className="block w-full">
+          <Header />
+          <main>TODO: {/* <Outlet/> */}</main>
+          <Footer />
+        </div>
+      </div>
     </>
-  )
+  ) : null;
 }
 
-export default App
+export default App;
