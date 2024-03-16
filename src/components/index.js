8b38904994e11a0604bd9438ea2288f285d91645
Harshit Soni <kratin67soni@gmail.com>
2024-03-16 16:42:43 +0530

    worked on setting up router, pages and components

--- src/components/index.js
+++ src/components/index.js
@@ -8,6 +8,7 @@ import Signup from "./Signup";
 import Login from "./Login";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
 
 export {
   Header,
@@ -19,4 +20,5 @@ export {
   Login,
   PostForm,
   PostCard,
+  AuthLayout,
 };
